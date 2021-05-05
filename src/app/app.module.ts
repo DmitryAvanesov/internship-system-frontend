81eb2c557ad6666ac2f2a256d843cd72322dcd7b
Dima <dima.avanesov@yandex.ru>
2021-05-05 15:23:13 +0700

    Added header titles according to routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { LayoutComponent } from '@core/layout/layout/layout.component';
-import { HeaderComponent } from '@core/layout/header/header.component';
-import { MenuComponent } from '@core/layout/menu/menu.component';
+import { LayoutComponent } from '@core/layout/layout.component';
 import { environment } from '../environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { StoreModule } from '@ngrx/store';
@@ -16,7 +14,7 @@ import { appReducers } from './store/root.reducer';
 import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
-  declarations: [AppComponent, LayoutComponent, HeaderComponent, MenuComponent],
+  declarations: [AppComponent, LayoutComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
