0296672fa7da9f03526a323b9e6006002caf1403
Dima <dima.avanesov@yandex.ru>
2021-05-12 12:14:22 +0700

    Added a model for student props

--- src/app/store/students/students.selectors.ts
+++ src/app/store/students/students.selectors.ts
@@ -2,14 +2,13 @@ import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { AppState } from '../root.reducer';
 import { StudentsState } from './students.state';
 import { studentsAdapter } from './students.reducer';
+import { SelectStudentProps } from '@store/students/models/select-student-props.model';
 
-const selectStudents = createFeatureSelector<AppState, StudentsState>(
-  'students'
-);
+const selectStudents =
+  createFeatureSelector<AppState, StudentsState>('students');
 
-export const { selectAll: selectAllStudents } = studentsAdapter.getSelectors(
-  selectStudents
-);
+export const { selectAll: selectAllStudents } =
+  studentsAdapter.getSelectors(selectStudents);
 
 export const selectStudentsLoading = createSelector(
   selectStudents,
@@ -18,5 +17,5 @@ export const selectStudentsLoading = createSelector(
 
 export const selectStudent = createSelector(
   selectStudents,
-  (state, props) => state.entities[props.id]
+  (state: StudentsState, props: SelectStudentProps) => state.entities[props.id]
 );
