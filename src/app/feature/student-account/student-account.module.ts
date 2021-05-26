6991fce0313c770a88923b23b6c5cef0c37675ec
Dima <dima.avanesov@yandex.ru>
2021-05-26 11:48:05 +0700

    Added some student account layout

--- src/app/feature/student-account/student-account.module.ts
+++ src/app/feature/student-account/student-account.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {StudentAccountRouting} from './student-account.routing';
-import {SharedModule} from '@shared/shared.module';
-
-
+import { StudentAccountRouting } from './student-account.routing';
+import { SharedModule } from '@shared/shared.module';
+import { StudentAccountComponent } from '@student-account/pages/student-account/student-account.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    StudentAccountRouting,
-    SharedModule,
-  ]
+  declarations: [StudentAccountComponent],
+  imports: [CommonModule, StudentAccountRouting, SharedModule],
 })
-export class StudentAccountModule { }
+export class StudentAccountModule {}
